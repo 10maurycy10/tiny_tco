a7308950bdf89af122186ea2c5ebce10fdc49398
mz <10maurycy10@gmail.com>
2021-01-07 11:36:52 -0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# tiny_tco
+
+A tiny dirt simple no_std tail call optimization library.
+
+## how to use
+
+    // y is the acoumulator for the value
+    let fact = tco(|(x,y): (i32,i32)|
+    	if (x == 0) {
+    		// if we have reached 0 return computed value
+    		TCO::Ret(y)
+    	} else {
+    		// reduce x by 1, and multiplyx value by x
+    		TCO::Rec((x-1,y*x)
+    	},
+    );
+    assert_eq!(f((3,1)),6);
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#![no_std]
+
+/// A helper for TCO
+pub enum TCO<REC, RET> {
+	/// Recall function
+	Rec(REC),
+	/// Return a value
+	Ret(RET),
+}
+
+/// Construct a fn(A) -> B from a function that returns Fn(A) -> TCO<A,B>
+pub fn tco<A,B>(fun: fn(A) -> TCO<A,B>) -> impl Fn(A) -> B {
+	move |p: A| {
+		let mut call = TCO::Rec(p);
+		while let TCO::Rec(i) = call {
+			call = fun(i);
+		}
+		match call {
+			TCO::Rec(_) => unreachable!(),
+			TCO::Ret(a) => a
+		}
+	}
+}
+
+/// use tail calls to compute factorial.
+/// converted to optimized function with tco
+/// this avoids stack overflows
+#[test]
+fn test_tco() {
+	// y is an acumulator for result
+	let f = tco(|(x,y): (i32,f64)|
+		if (x == 0) {
+			// if we have reached 0 return computed value
+			TCO::Ret(y)
+		} else {
+			// reduce x by 1, and multiplyx value by x
+			TCO::Rec((x-1,y*(x as f64)))
+		},
+	);
+	assert_eq!(f((3,1.0)),6.0);
+	f((100000,1.0));
+}
